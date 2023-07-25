2d5f2cf8afb35006875dbc1b877c69e2161e331c
vineethy727 <vineethy727@gmail.com>
2023-07-25 22:03:01 +0530

    eureka-server for customer and vaccine

---
+++ src/main/java/com/server/Application.java
@@ -0,0 +1,22 @@
+package com.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+	//eureka service
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	//eureka-server
+	//citizen-service
+	//vaccine-service
+	
+	
+	
+}
